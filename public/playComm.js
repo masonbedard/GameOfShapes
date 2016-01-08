efc5f3adcd029732f0103f039f8de602344070a0
masonbedard <mbedard@terpmail.umd.edu>
2016-01-08 14:06:36 -0500

    Revert "Code clean-up"

---
+++ public/playComm.js
@@ -0,0 +1,93 @@
+var playComm = {};
+playComm.controlled = false;
+playComm.name = null;
+
+playComm.startCanvas = function() {
+  JQbody.html("<canvas id='view' tabindex='1'></canvas>");
+  var canvas = document.getElementById('view');
+  canvas.focus();
+  playComm.processingInstance = new Processing(canvas, view);
+};
+/*
+JQdoc.on('click', '#start', function() {
+  playComm.startCanvas();
+});
+*/
+
+JQdoc.on('click', '#uncontplayagain', function() {
+  playComm.startCanvas();
+});
+
+/*
+
+socket.on('controlled', function() {
+  JQbody.html("<div class='a'></div>" +
+    "<div class='option'>TITLE</div>" + 
+    "<div class='b'></div>" +
+    "<div class='option'>id " + playComm.roomID + "</div>" +
+    "<div class='b'></div>" +
+    "<div class='option'>use controller</div>" +
+    "<div class='b'></div>" +
+    "<div class='option'>to start</div>" +
+    "<div class ='a'></div>"
+  );
+  playComm.controlled = true;
+});
+socket.on('mobile says play again', function() {
+  playComm.startCanvas();
+});
+
+socket.on('mobile says start', function() {
+  playComm.startCanvas();
+});
+
+socket.on('display high scores', function(data) {
+  playComm.playAgainCont(data.scores);
+});
+
+socket.on('display desk play again', function(data) {
+  playComm.playAgainUncont(data.scores);
+});
+*/
+
+
+playComm.playAgainCont = function(scores) {
+  playComm.processingInstance.exit();
+  playComm.processingInstace = null;
+  
+  var page = "<div class='a'></div><div class='option'>TITLE</div><div class='b'></div><div class='option'>game over</div><div class='b'></div><div class='longtext'>leaders<br>";
+  //var page = "<div class='option'>TITLE</div><div class='b'></div><div id='longtext'>leaders<br>";
+  for (var i = 0; i < scores.length; i++) {
+    page += scores[i].name + " " + scores[i].score + "<br>";
+  }
+  page += "</div><div class='b'></div><div class='option button'>more projects</div><div class='a'></div>";
+  
+  JQbody.html(page);
+};
+
+playComm.playAgainUncont = function(scores) {
+  playComm.processingInstance.exit();
+  playComm.processingInstace = null;
+  
+  var page = "<div class='a'></div><div class='option'>TITLE</div><div class='b'></div><div class='option button' id='uncontplayagain'>play again</div><div class='b'></div><div class='longtext'>leaders<br>";
+  //var page = "<div class='option'>TITLE</div><div class='b'></div><div id='longtext'>leaders<br>";
+  for (var i = 0; i < scores.length; i++) {
+    page += scores[i].name + " " + scores[i].score + "<br>";
+  }
+  page += "</div><div class='b'></div><div class='option button'>more projects</div><div class='a'></div>";
+  
+  JQbody.html(page);
+};
+
+playComm.tellContGameOver = function(score) {
+  socket.emit('game over', {roomID: playComm.roomID, 'score': score});
+};
+
+playComm.submitScore = function(score) {
+  if (playComm.name === null || playComm.name === '') {
+    playComm.name = prompt('enter name to submit score');
+  }
+  console.log(playComm.name);
+  socket.emit('name entered from desk', {roomID: playComm.roomID, 'playerName':playComm.name, 'score': score});
+}
+
---
+++ src/js/game/controller.js
@@ -0,0 +1,169 @@
+var initController = function(pjs) {
+
+  var halfScreenWidth;
+
+  var leftStick = {};
+  var rightStick = {};
+
+  leftStick.id;
+  rightStick.id;
+
+  leftStick.origin = new pjs.PVector();
+  rightStick.origin = new pjs.PVector();
+
+  leftStick.current = new pjs.PVector();
+  rightStick.current = new pjs.PVector();
+
+  leftStick.active = false;
+  rightStick.active = false;
+
+  var outerRadius;
+  var innerRadius;
+  var maxStickTravel;
+  
+  pjs.setup = function() {
+
+    pjs.size(pjs.screenWidth,pjs.screenHeight);
+    halfScreenWidth = pjs.screenWidth / 2;
+    pjs.smooth();
+    pjs.noStroke();
+    pjs.frameRate(60);
+    outerRadius = 120;
+    //outerRadius = pjs.screenWidth / 4;
+    innerRadius = outerRadius / 4 * 3;
+    maxStickTravel = outerRadius / 4;
+
+  };
+
+
+  pjs.draw = function() {
+    pjs.background(pjs.color(221,223,216));
+
+    if (leftStick.active || rightStick.active) {
+      if (leftStick.active === true) {
+        pjs.fill(50,50,50);
+        pjs.ellipse(leftStick.origin.x, leftStick.origin.y, outerRadius, outerRadius);
+        pjs.fill(100, 100, 100);
+        pjs.ellipse(leftStick.current.x, leftStick.current.y, innerRadius, innerRadius);
+      }
+      if (rightStick.active === true) {
+        pjs.fill(50,50,50);
+        pjs.ellipse(rightStick.origin.x, rightStick.origin.y, outerRadius, outerRadius);
+        pjs.fill(100, 100, 100);
+        pjs.ellipse(rightStick.current.x, rightStick.current.y, innerRadius, innerRadius);
+      }
+    }
+
+    else {
+      pjs.background(pjs.color(221,223,216));
+      pjs.noLoop();
+    }
+
+  };
+
+  pjs.touchStart = function(e) {
+
+    e.preventDefault();
+    for (var i = 0; i < e.changedTouches.length; i++) {
+      var tempTouch = e.changedTouches[i];
+      if (leftStick.active === false && tempTouch.clientX < halfScreenWidth) {
+        leftStick.origin.set(tempTouch.clientX, tempTouch.clientY, 0);
+        leftStick.current.set(tempTouch.clientX, tempTouch.clientY, 0);
+        leftStick.id = tempTouch.identifier;       
+        leftStick.active = true;
+        pjs.loop();
+      }
+      else if (rightStick.active === false) {
+        rightStick.origin.set(tempTouch.clientX, tempTouch.clientY, 0);
+        rightStick.current.set(tempTouch.clientX, tempTouch.clientY, 0);
+        rightStick.id = tempTouch.identifier;       
+        rightStick.active = true;
+        pjs.loop();
+      }
+    }
+  };
+
+  pjs.touchEnd = function(e) {
+
+   e.preventDefault();
+
+    for (var i = 0; i < e.changedTouches.length; i++) {
+
+/*
+      if (leftStick.id === e.changedTouches[i].identifier) {
+        leftStick.active = false;
+        socket.emit('left stick stop', {roomID: contComm.roomID});
+      }
+      else if (rightStick.id === e.changedTouches[i].identifier) {
+        rightStick.active = false;
+        socket.emit('right stick stop', {roomID: contComm.roomID});
+      }
+      */
+
+    }
+  };
+
+  pjs.touchMove = function(e) {
+
+    e.preventDefault();
+
+    for (var i = 0; i < e.changedTouches.length; i++) {
+
+      var tempTouch = e.changedTouches[i];
+      var tempVector = new pjs.PVector(tempTouch.clientX, tempTouch.clientY);
+
+      if (leftStick.id === tempTouch.identifier) {
+        var currVector = pjs.PVector.sub(tempVector, leftStick.origin);
+        var currDist = currVector.mag();
+        var activeDist = (currDist < maxStickTravel ? currDist : maxStickTravel);
+        var currX, currY;
+
+        if (currDist !== 0) {
+          var currAngle = Math.atan(currVector.y/currVector.x);
+          if (currVector.x < 0) {
+            currAngle += Math.PI;
+          }
+          currX = Math.cos(currAngle) * (activeDist) + leftStick.origin.x;
+          currY = Math.sin(currAngle) * (activeDist) + leftStick.origin.y;
+        }
+        else {
+          currX = leftStick.origin.x;
+          currY = leftStick.origin.y;
+        }
+        leftStick.current.set(currX, currY, 0);
+
+        var distPercent = activeDist / maxStickTravel;
+
+        currVector.normalize();
+
+       // socket.emit('left stick move', {roomID: contComm.roomID, 'currVector': currVector, 'distPercent': distPercent});
+
+      } else if (rightStick.id === tempTouch.identifier) {
+        var currVector = pjs.PVector.sub(tempVector, rightStick.origin);
+        var currDist = currVector.mag();
+        var activeDist = (currDist < maxStickTravel ? currDist : maxStickTravel);
+        var currX, currY;
+        
+        if (currDist !== 0) {
+          var currAngle = Math.atan(currVector.y/currVector.x);
+          if (currVector.x < 0) {
+            currAngle += Math.PI;
+          }
+          currX = Math.cos(currAngle) * (activeDist) + rightStick.origin.x;
+          currY = Math.sin(currAngle) * (activeDist) + rightStick.origin.y;
+        }
+        else {
+          currX = rightStick.origin.x;
+          currY = rightStick.origin.y;
+        }
+        rightStick.current.set(currX, currY, 0);
+
+        //socket.emit('right stick move', {roomID: contComm.roomID, vel: currVector});
+
+      }
+    }
+  };
+
+};
+
+module.exports = initController;
